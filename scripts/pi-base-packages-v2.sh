401be95e9cb806bdf09f3c52a83f283b3798224c
dossjj <justindoss94@gmail.com>
2018-01-17 04:25:58 +0000

    new: created scripts dir - base packages upload

---
+++ scripts/pi-base-packages-v2.sh
@@ -0,0 +1,37 @@
+﻿#!/bin/bash
+#pi base package installation
+
+#update
+echo "\n\n  performing update....\n\n"
+sudo apt-get update -y
+echo "\n\n  update complete\n\n"
+
+#upgrade
+echo "\n\n  performing upgrade......\n\n"
+sudo apt-get upgrade -y
+echo "\n\n  upgrade complete\n\n"
+
+#install apache
+echo "\n\n  installing apache......\n\n"
+sudo apt-get install apache2 apache2-utils -y
+echo "\n\n  apache installed\n\n"
+
+#install php
+echo "\n\n  installing php.........\n\n"
+sudo apt-get install php -y
+echo "\n\n  php installed\n\n"
+
+#install deluge
+echo "\n\n installing deluge.......\n\n"
+sudo apt-get install deluge -y
+echo "\n\n deluge installed\n\n"
+
+#install openvpn
+echo "\n\n  installing openvpn......\n\n"
+sudo apt-get install openvpn -y
+echo "\n\n  openvpn installed\n\n"
+
+#install vim
+echo "\n\n  installing vim......\n\n"
+sudo apt-get install vim -y
+echo "\n\n  vim installed\n\n"
---
+++ scripts/pi-base-packages.sh
@@ -0,0 +1,37 @@
+﻿#!/bin/bash
+#pi base package installation
+
+#update
+echo "performing update"
+sudo apt-get update -y
+echo "update complete"
+
+#upgrade
+echo "performing upgrade"
+sudo apt-get upgrade -y
+echo "upgrade complete"
+
+#install apache
+echo "installing apache"
+sudo apt-get install apache2 apache2-utils -y
+echo "apache installed"
+
+#install php
+echo "installing php"
+sudo apt-get install php -y
+echo "php installed"
+
+#install deluge
+echo "installing deluge"
+sudo apt-get install deluge -y
+echo "deluge installed"
+
+#install openvpn
+echo "installing vpn"
+sudo apt-get install openvpn -y
+echo "openvpn installed"
+
+#install vim
+echo "installing vim"
+sudo apt-get install vim -y
+echo "vim installed"
